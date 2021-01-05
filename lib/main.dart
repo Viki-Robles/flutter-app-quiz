0293815ab2d643d9655757df48e7ebbfb67831fe
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2021-01-05 01:51:22 +0000

    feat:building a flutter quiz app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     home: Scaffold(
+       body: Center(
+         child: Platform.isAndroid
+             ? Switch(value: true, onChanged: (v) => null)
+             : CupertinoSwitch(value: true, onChanged: (v) => null),
+       ),
+     ),
+   );
+ }
+}
